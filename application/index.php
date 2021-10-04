687b437ce9214f995af7c0d3f7cd0c9d327a0a6f
EtienneV <etienne.vaytilingom@gmail.com>
2021-10-04 13:12:57 +0400

    TP2 - Integrer Bootstrapp

--- application/index.php
+++ application/index.php
@@ -1,15 +1,3 @@
-<?php require_once '../comfpl/main.php';?>
-<html> 
-<head> 
-<title>Team up - Index</title> 
-<?php require_once 'phpinclude/commonmeta.php';?> 
-<?php require_once 'phpinclude/commoncss.php';?> 
-<?php require_once 'phpinclude/theme.php';?> 
-<?php require_once 'phpinclude/commonscript.php';?>
-</head> 
-<body> 
-<?php require_once 'phpinclude/navbar.php';?> 
-    <h2>Bienvenue dans Team Up</h2> 
-    <h2>Page application</h2> 
-</body> 
-</html>
+<?php 
+require_once 'config.php'; 
+require_once 'views/_layout.php';?>
