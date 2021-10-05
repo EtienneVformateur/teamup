63669337ac2869f4ff13c5ade367e11894d5718f
EtienneV <etienne.vaytilingom@gmail.com>
2021-10-05 10:56:38 +0400

    TP3

--- application/phpinclude/navbar.php
+++ application/phpinclude/navbar.php
@@ -1,48 +1,4 @@
 <?php
-// menu (première version)
-$menu = array();
-$menu[0] = array(
-    "route" => "#",
-    "label" => "Accueil",
-    "tag" => "",
-    "icon" => "home"
-);
-$menu[1] = array(
-    "route" => "#",
-    "label" => "Utilisateurs",
-    "tag" => "",
-    "icon" => "avatar"
-);
-$menu[2] = array(
-    "route" => "#",
-    "label" => "Demandes",
-    "tag" => "",
-    "icon" => "collective"
-);
-$menu[3] = array(
-    "route" => "#",
-    "label" => "Agenda",
-    "tag" => "",
-    "icon" => "calendar"
-);
-$menu[4] = array(
-    "route" => "#",
-    "label" => "T&acirc;ches",
-    "tag" => "",
-    "icon" => "coding"
-);
-$menu[5] = array(
-    "route" => "#",
-    "label" => "Messages",
-    "tag" => "",
-    "icon" => "chat"
-);
-$menu[6] = array(
-    "route" => "#",
-    "label" => "Rapports",
-    "tag" => "",
-    "icon" => "analysis"
-);
 
 // menu (seconde version)
 $menu_json=file_get_contents("menu.json", FILE_USE_INCLUDE_PATH);
